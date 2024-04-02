85776739cd8c3bb3c05da9a20fac9e5723f4d40e
dusanlecic <dusan.lecic5@gmail.com>
2024-04-02 23:21:34 +0200

    add: some gradient logic to begin

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,28 @@
-fn main() {
-    println!("Rustracer");
+use std::io::{self, Write, Result};
+
+const IMAGE_WIDTH: u32 = 256;
+const IMAGE_HEIGHT: u32 = 256;
+
+fn main() -> Result<()> {
+    let stdout = io::stdout();
+    let mut handle = stdout.lock();
+
+    writeln!(handle, "P3\n{} {}\n255", IMAGE_WIDTH, IMAGE_HEIGHT)?;
+    
+    for col in 0..IMAGE_HEIGHT {
+        for row in 0..IMAGE_WIDTH {
+            let r = col as f32 / (IMAGE_WIDTH - 1) as f32;
+            let g = col as f32 / (IMAGE_HEIGHT - 1) as f32;
+            let b = 0.0;
+
+            let ir = (255.999 * r) as i32;
+            let ig = (255.999 * g) as i32;
+            let ib = (255.999 * b) as i32;
+
+            writeln!(handle, "{} {} {}", ir, ig, ib)?;
+
+        }
+    }
+
+    Ok(())
 }
