f7a003d070f0e7310232966b1ee749c6100c353b
dusanlecic <dusan.lecic5@gmail.com>
2024-04-02 23:32:55 +0200

    add: basic logger

--- src/main.rs
+++ src/main.rs
@@ -10,6 +10,7 @@ fn main() -> Result<()> {
     writeln!(handle, "P3\n{} {}\n255", IMAGE_WIDTH, IMAGE_HEIGHT)?;
     
     for col in 0..IMAGE_HEIGHT {
+        eprintln!("\rScanlines remaining: {} ", IMAGE_HEIGHT - col);
         for row in 0..IMAGE_WIDTH {
             let r = row as f32 / (IMAGE_WIDTH - 1) as f32;
             let g = col as f32 / (IMAGE_HEIGHT - 1) as f32;
