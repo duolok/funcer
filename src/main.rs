976ad597db8d15912881c2ea00d6939e0a154621
dusanlecic <dusan.lecic5@gmail.com>
2024-04-03 09:11:21 +0200

    refactor: code is now more managable | write function extracted

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,19 @@
+mod vec3;
+use crate::vec3::Vec3;
 use std::io::{self, Write, Result};
+
+pub type Color = Vec3;
 
 const IMAGE_WIDTH: u16 = 256;
 const IMAGE_HEIGHT: u16 = 256;
+
+pub fn write_color(color: Vec3) {
+    let r = (255.999 * color.x()) as i32;
+    let g = (255.999 * color.y()) as i32;
+    let b = (255.999 * color.z()) as i32;
+
+    println!("{} {} {}", r, g, b);
+}
 
 fn main() -> Result<()> {
     let stdout = io::stdout();
@@ -12,15 +24,11 @@ fn main() -> Result<()> {
     for col in 0..IMAGE_HEIGHT {
         eprintln!("\rScanlines remaining: {} ", IMAGE_HEIGHT - col);
         for row in 0..IMAGE_WIDTH {
-            let r = row as f32 / (IMAGE_WIDTH - 1) as f32;
-            let g = col as f32 / (IMAGE_HEIGHT - 1) as f32;
-            let b = 0.0;
+            let color = Color::new((row as f32 / (IMAGE_WIDTH - 1) as f32).into(),
+                               (col as f32 / (IMAGE_HEIGHT - 1) as f32).into(),
+                               0.0);
+            write_color(color);
 
-            let ir = (255.999 * r) as i32;
-            let ig = (255.999 * g) as i32;
-            let ib = (255.999 * b) as i32;
-
-            writeln!(handle, "{} {} {}", ir, ig, ib)?;
         }
     }
 
