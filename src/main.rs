eaeeca1d47495236577a286963305b7508c3ced0
duolok <dusan.lecic5@example.com>
2024-04-17 21:38:40 +0200

    add: integrated ray

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,16 @@
 mod vec3;
-use crate::vec3::Vec3;
+mod ray;
+
+
 use std::io::{self, Write, Result};
+use vec3::Vec3;
+use ray::{Ray, Point3};
 
 pub type Color = Vec3;
 
-const IMAGE_WIDTH: u16 = 256;
-const IMAGE_HEIGHT: u16 = 256;
+const ASPECT_RATIO: f64 = 16.0 / 9.0;
+const IMAGE_WIDTH: u64 = 256;
+const IMAGE_HEIGHT: u64 = ((256 as f64) / ASPECT_RATIO) as u64;
 
 pub fn write_color(color: Vec3) {
     let r = (255.999 * color.x()) as i32;
@@ -15,19 +20,40 @@ pub fn write_color(color: Vec3) {
     println!("{} {} {}", r, g, b);
 }
 
+fn ray_color(r: &Ray) -> Color {
+    let unit_direciton = r.direction().normalized();
+    let t = 0.5 * (unit_direciton.y() + 1.0) ;
+    (1.0 - t) * Color::new(1.0, 1.0, 1.0) + t * Color::new(0.5, 0.7, 1.0)
+}
+
 fn main() -> Result<()> {
     let stdout = io::stdout();
     let mut handle = stdout.lock();
+    
+    let viewport_height = 2.0;
+    let viewport_width = ASPECT_RATIO * viewport_height;
+    let focal_length = 1.0;
+
+    let origin = Point3::new(0.0, 0.0, 0.0);
+    let horizontal = Vec3::new(viewport_width, 0.0, 0.0);
+    let vertical = Vec3::new(0.0, viewport_height, 0.0);
+    let lower_left_corner = origin - horizontal / 2.0 - vertical / 2.0 - Vec3::new(0.0, 0.0, focal_length);
+
+
 
     writeln!(handle, "P3\n{} {}\n255", IMAGE_WIDTH, IMAGE_HEIGHT)?;
     
     for col in 0..IMAGE_HEIGHT {
         eprintln!("\rScanlines remaining: {} ", IMAGE_HEIGHT - col);
+
         for row in 0..IMAGE_WIDTH {
-            let color = Color::new((row as f32 / (IMAGE_WIDTH - 1) as f32).into(),
-                               (col as f32 / (IMAGE_HEIGHT - 1) as f32).into(),
-                               0.0);
-            write_color(color);
+            let u = (row as f64) / ((IMAGE_WIDTH - 1) as f64);
+            let v = (col as f64) / ((IMAGE_HEIGHT - 1) as f64);
+            let ray = Ray::new(&origin, &(lower_left_corner + u * horizontal + v * vertical - origin));
+    
+            let pixel_color = ray_color(&ray);
+
+            write_color(pixel_color);
 
         }
     }
