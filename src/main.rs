4965503e9fd3ef978be41cefcfe3b539ef87de1a
duolok <dusan.lecic5@gmail.com>
2024-04-30 19:48:23 +0200

    add: updated main to draw world elements

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,14 @@
 mod vec3;
 mod ray;
-
+mod hit;
+mod sphere;
 
 use std::io::{self, Write, Result};
 use vec3::Vec3;
 use ray::{Ray, Point3};
+use hit::{Hit, World};
+use sphere::Sphere;
+
 
 pub type Color = Vec3;
 
@@ -22,33 +26,36 @@ pub fn write_color(color: Vec3) {
 
 fn hit_sphere(center: Point3, radius: f64, r: &Ray) -> f64 {
     let oc = r.origin() - center;
-    let a = r.direction().dot(r.direction());
-    let b = 2.0 * oc.dot(r.direction());
-    let c = oc.dot(oc) - radius * radius;
-    let discriminant = b * b - 4.0 * a * c;
+    let a = r.direction().length().powi(2);
+    let half_b = oc.dot(r.direction());
+    let c = oc.length().powi(2) - radius * radius;
+    let discriminant = half_b * half_b - a * c;
+
 
     if discriminant < 0.0 {
         -1.0
     } else {
-        (-b - discriminant.sqrt()) / (2.0 * a)
+        (-half_b - discriminant.sqrt()) / (2.0 * a)
     }
 }
 
-fn ray_color(r: &Ray) -> Color {
-    let t = hit_sphere(Point3::new(0.0, 0.0, -1.0), 0.5, r);
-    if t > 0.0 {
-        let n = (r.at(t) - Point3::new(0.0, 0.0, -1.0)).normalized();
-        return 0.5 * Color::new(n.x() + 1.0, n.y() + 1.0 , n.z() + 1.0);
+fn ray_color(r: &Ray, world: &World) -> Color {
+    if let Some(rec) = world.hit(r, 0.0, f64::INFINITY) {
+        0.5 * (rec.normal + Color::new(1.0, 1.0, 1.0))
+    } else {
+        let unit_direciton = r.direction().normalized();
+        let t = 0.5 * (unit_direciton.y() + 1.0);
+        (1.0 - t) * Color::new(1.0, 1.0, 1.0) + t * Color::new(0.5, 0.7, 1.0)
     }
-
-    let unit_direciton = r.direction().normalized();
-    let t = 0.5 * (unit_direciton.y() + 1.0);
-    (1.0 - t) * Color::new(1.0, 1.0, 1.0) + t * Color::new(0.5, 0.7, 1.0)
 }
 
 fn main() -> Result<()> {
     let stdout = io::stdout();
     let mut handle = stdout.lock();
+
+    let mut world = World::new();
+    world.push(Box::new(Sphere::new(Point3::new(0.0, 0.0, -1.0), 0.5)));
+    world.push(Box::new(Sphere::new(Point3::new(0.0, -100.5, -1.0), 100.0)));
     
     let viewport_height = 2.0;
     let viewport_width = ASPECT_RATIO * viewport_height;
@@ -63,7 +70,7 @@ fn main() -> Result<()> {
 
     writeln!(handle, "P3\n{} {}\n255", IMAGE_WIDTH, IMAGE_HEIGHT)?;
     
-    for col in 0..IMAGE_HEIGHT {
+    for col in (0..IMAGE_HEIGHT).rev() {
         eprintln!("\rScanlines remaining: {} ", IMAGE_HEIGHT - col);
 
         for row in 0..IMAGE_WIDTH {
@@ -71,7 +78,7 @@ fn main() -> Result<()> {
             let v = (col as f64) / ((IMAGE_HEIGHT - 1) as f64);
             let ray = Ray::new(&origin, &(lower_left_corner + u * horizontal + v * vertical - origin));
     
-            let pixel_color = ray_color(&ray);
+            let pixel_color = ray_color(&ray, &world);
 
             write_color(pixel_color);
 
